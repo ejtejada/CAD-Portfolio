d8f75682ce17f1b4228699c7b0d5f6f7a9de0e99
ejtejada <31432911+ejtejada@users.noreply.github.com>
2020-05-11 00:41:50 -0700

    Mass Properties
    Adding mass properties and and preparing for motor animation.

--- getAssembleyVar.c
+++ getAssembleyVar.c
@@ -1,3 +1,8 @@
 = "HoleMountDiameter@PrintingPress.Assembly"
 = "RivetLength@PrintingPress.Assembly"
 ="baseFrameThickness@BaseFrame.sldprt"
+
+Adjustment to press arm = 54cm + 2.462cm;
+Adjustments to EvenLongerLink = = 60cm+23.819cm
+GearCut Radius = 39.09424624cm
+OG gear thickness = 1.27cm
