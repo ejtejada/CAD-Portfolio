b18e9811ffd373d8a60e56b4934523d76a80dad9
ejtejada <31432911+ejtejada@users.noreply.github.com>
2020-05-12 02:19:37 -0700

    Including Stress Simulation
    Using public data on Thornel Mat VMA Carbon_Fiber

--- getAssembleyVar.c
+++ getAssembleyVar.c
@@ -6,3 +6,5 @@ Adjustment to press arm = 54cm + 2.462cm;
 Adjustments to EvenLongerLink = = 60cm+23.819cm
 GearCut Radius = 39.09424624cm
 OG gear thickness = 1.27cm
+Mass of system =1.5 Metric Tons
+Full force if whole system on one arm = 14700 N
